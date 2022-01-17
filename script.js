ca1cdd3d8387e303039130a9f465c711b78e2b83
VasifAliyev <vasif.alievv@gmail.com>
2022-01-17 19:10:26 +0400

    second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const siyahi = ['Paper.png','Rock.png','Scissors.png']
+
+// DOM - Document Object Model
+
+const kagiz = document.getElementById("paper")
+const dash = document.getElementById("rock")
+const qaychi = document.getElementById("scissors")
+
+// komputerin qerari ve nectice olan hisse
+const komputerinSecimi = document.getElementById("komputer-secim")
+const neticeniYaz = document.getElementById("netice")
+
+// hadiseler - onclick, onmouseover, onchange
+kagiz.onclick = seciminiEle
+dash.onclick = seciminiEle
+qaychi.onclick = seciminiEle
+
+function seciminiEle() {
+
+}
+
+function randomSecim() {
+    
+}
